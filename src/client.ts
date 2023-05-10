862e4c5a4f22a62555b44619f58f193a73a15f16
Loren 🤓 <lorensr@gmail.com>
2023-05-10 12:33:19 -0400

    Add signal and sleeping to workflow

--- src/client.ts
+++ src/client.ts
@@ -1,18 +1,23 @@
 import { Client } from '@temporalio/client';
-import { example } from './workflows';
 import { nanoid } from 'nanoid';
+import wait from 'waait';
+import { counter, incrementSignal } from './workflows';
 
 async function run() {
   const client = new Client();
 
-  const handle = await client.workflow.start(example, {
+  const handle = await client.workflow.start(counter, {
     args: ['Temporal'],
     taskQueue: 'hello-world',
     workflowId: 'workflow-' + nanoid(),
   });
   console.log(`Started workflow ${handle.workflowId}`);
 
-  console.log(await handle.result());
+  await handle.signal(incrementSignal, 10);
+  await wait(1500);
+  await handle.signal(incrementSignal, 20);
+  await wait(2000);
+  await handle.signal(incrementSignal, 12);
 }
 
 run().catch((err) => {
--- src/workflows.ts
+++ src/workflows.ts
@@ -1,10 +1,27 @@
-import { proxyActivities } from '@temporalio/workflow';
+import { defineSignal, proxyActivities, setHandler, sleep } from '@temporalio/workflow';
 import type * as activities from './activities';
 
 const { greet } = proxyActivities<typeof activities>({
   startToCloseTimeout: '1 minute',
 });
 
-export async function example(name: string): Promise<string> {
-  return await greet(name);
+export const incrementSignal = defineSignal<[number]>('increment');
+
+export async function counter(name: string): Promise<void> {
+  let total = 0;
+  let activity1: string, activity2: string; // eslint-disable-line prefer-const
+
+  setHandler(incrementSignal, (count) => {
+    total += count;
+  });
+
+  await sleep('1s');
+  activity1 = await greet(name);
+  await sleep('2s');
+  activity2 = await greet(name);
+
+  // don't complete the workflow
+  await new Promise(() => {
+    return;
+  });
 }
